a4695d992dc36c785bcf9ab2917444f6205cb371
Lowliie <lolipopkungz@gmail.com>
2022-09-06 00:17:10 +0700

    Init

---
+++ spec/atom-term-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomTermView from '../lib/atom-term-view';
+
+describe('AtomTermView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
